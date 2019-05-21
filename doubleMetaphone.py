e76629a1c18f7cf164133c5c175028a3360fcae7
Stacy Bridges <stcybrdgs@gmail.com>
2019-05-21 18:32:21 -0500

    git commit

---
+++ doubleMetaphone.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 21 16:41:11 2019
+
+@author: Stacy Bridges
+"""
+
+from metaphone import doublemetaphone
+from enum import Enum
+
+class Threshold(Enum):
+    WEAK = 0
+    NORMAL = 1
+    STRONG = 2
+
+def double_metaphone(value):
+    print(doublemetaphone(value))
+    return doublemetaphone(value)
+
+#(Primary Key = Primary Key) = Strongest Match
+#(Secondary Key = Primary Key) = Normal Match
+#(Primary Key = Secondary Key) = Normal Match
+#(Alternate Key = Alternate Key) = Minimal Match
+def double_metaphone_compare(tuple1,tuple2,threshold):
+    if threshold == Threshold.WEAK:
+        if tuple1[1] == tuple2[1]:
+            return True
+    elif threshold == Threshold.NORMAL:
+        if tuple1[0] == tuple2[1] or tuple1[1] == tuple2[0]:
+            return True
+    else:
+        if tuple1[0] == tuple2[0]:
+            return True
+    return False
+
+def main():
+    print('Done.')
+
+if__name__=='__main__' : main()
+    
+    
