0884fe46cc83a9f304a3910e1a6948668cb2c595
Stacy Bridges <stcybrdgs@gmail.com>
2019-05-23 14:59:51 -0500

    git commit

--- spacy_v1.py
+++ spacy_v1.py
@@ -10,18 +10,11 @@ import spacy
 from spacy.lang.en.examples import sentences
 
 def main():
-  products = open('products.txt', 'rt')
-  str = u"I can't imagine spending $3000 for a single bedroom apartment in N.Y.C."
-  
   nlp = spacy.load('en_core_web_sm')
-  sent = nlp(sentences[0])
-  str = nlp(str)
-  #prod = prod[0]
-  
-  print('Start --------------------------')
   
   # sent functions ----------------------  
-  print('setence example:\n')
+  print('setence example: ---------------------------')
+  sent = nlp(sentences[0])
   print(sent.text)
   
   for token in sent:
@@ -30,7 +23,9 @@ def main():
   print('\n')
   
   # str functions ----------------------  
-  print('string example:\n')
+  print('string example: ---------------------------')
+  sampleString = u"I can't imagine spending $3000 for a single bedroom apartment in N.Y.C."
+  str = nlp(sampleString)
   print(str.text)
   
   for token in str:
@@ -38,23 +33,37 @@ def main():
    
   print('\n')
   
-  # prod functions ---------------------
-  print('products example:\n')
-
+  # product file functions ---------------------
+  print('products example 1: ---------------------------')
+  infile = open('products_DescriptionOnly_short.csv', 'rt')
+  print(infile.read(), '\n')
+  
+  # reset cursor
+  infile.seek(0)
+  
   # start for
-  for line in products:
-    nextLine = line.rstrip()
-    nlpNextLine = nlp(nextLine)
-    
-    for token in nlpNextLine:
-        print(token.text, token.pos_, token.dep_)
+  for line in infile:
+      nextLine = line.rstrip()
+      # nextStr = nextLine
+      nlpStr = nlp(nextLine)
+     
+      for token in nlpStr:
+          print(token.text, token.pos_, token.dep_)
   
-    print('\n')
+      print('\n')
   # end for
-   
-  products.close()  
   
-  print('Done.')
+  infile.close()
+  
+  print('products example 2: ---------------------------')
+  infile = open('products_DescriptionOnly_short.csv', 'rt')
+  fData = infile.read()
+  nlpData = nlp(fData)
+  print(nlpData)
+  infile.close() 
+
+  # end program
+  print('\nDone.')
     
   
 if __name__ == '__main__' : main()
