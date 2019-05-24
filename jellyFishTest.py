b767fcd68916fd01ac72da22baa5bb1078ac2d85
Stacy Bridges <stcybrdgs@gmail.com>
2019-05-24 14:36:32 -0500

    git commit

--- jellyFishTest.py
+++ jellyFishTest.py
@@ -3,9 +3,9 @@
 Created on Thu May 23 20:33:36 2019
 @author: Owner
 
-testing jellyfish
+testing jellyfish functions
+project: python approximate string matching
 """
-
 import jellyfish
 
 def main():
@@ -14,7 +14,10 @@ def main():
     str1 = u'Jellyfish' 
     str2= u'Smellyfish'
     
-    # test Phonetic Encoding ------------------------------
+    
+    # test Phonetic Encoding
+    print('\nPhonetic Encoding ----------------------------')
+    
     # Metaphone
     r1 = jellyfish.metaphone(str1)
     r2 = jellyfish.metaphone(str2)
@@ -35,16 +38,27 @@ def main():
     r2 = jellyfish.match_rating_codex(str2)
     print('Match Rating Codex: ', r1, ", ", r2)
     
-    # test Stemming ---------------------------------------
+    
+    # test Stemming
+    print('\nStemming -------------------------------------')
+    pStr1 = u'Jellyfished'
+    pStr2 = u'Smellyfishing'
+    r1 = jellyfish.porter_stem(str1)
+    r2 = jellyfish.porter_stem(str2)
+    print('Porter Stemmer: ', r1, ", ", r2)
     
     
-    # test String Comparison ------------------------------
+    # test String Comparison
+    print('\nString Comparisons ---------------------------')
+    
     # Levenshtein Distance
     r = jellyfish.levenshtein_distance(str1, str2)
     print('Levenshtein Distance: ', r)
 
-    # Damerau-LevenshteinDistance
-        
+    # Damerau-Levenshtein Distance
+    r = jellyfish.damerau_levenshtein_distance(str1, str2)
+    print('Damerau-Levenshtein Distance: ', r)
+    
     # Hamming Distance
     result = jellyfish.hamming_distance(str1, str2)
     print('Hamming Distance: ', r)
@@ -60,8 +74,7 @@ def main():
     # Match Rating Approach (comparison)
     r = jellyfish.match_rating_comparison(str1, str2)
     print('Match Rating Comparison: ', r)
- 
-    
+     
     
     
     # end program
