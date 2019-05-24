2e8aa6e0351cbfd96946c08d27c60157512f4375
Stacy Bridges <stcybrdgs@gmail.com>
2019-05-24 18:35:05 -0500

    git commit

---
+++ spacy_3_vocabLexemeStringStore.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 22 2019
+@author: Stacy Bridges
+
+spaCy Data Structures:
+shared vocabulary, lexemes, and string store
+"""
+import spacy
+from spacy.matcher import Matcher
+
+def main():
+  nlp = spacy.load('en_core_web_sm')
+  
+  
+  
+  # end program
+  print('\nDone.')
+    
+  
+if __name__ == '__main__' : main()
+    
+    
