b8e0dd1f6f5e9e972a73a23eaade3a2b63069b55
Stacy Bridges <stcybrdgs@gmail.com>
2019-05-23 20:45:07 -0500

    git commit

--- jellyFishTest.py
+++ jellyFishTest.py
@@ -10,14 +10,18 @@ import jellyfish
 
 def main():
     lv = jellyfish.levenshtein_distance(u'jellyfish', u'smellyfish')
-    jaro = jellyfish.jaro_distance(u'jellyfish', u'smellyfish')
+    jd = jellyfish.jaro_distance(u'jellyfish', u'smellyfish')
+    jwd = jellyfish.jaro_winkler(u'jellyfish', u'smellyfish')
+    hd = jellyfish.hamming_distance(u'jellyfish', u'smellyfish')
     mp = jellyfish.metaphone(u'Jellyfish')
     sdx = jellyfish.soundex(u'Jellyfish')
     nys = jellyfish.nysiis(u'Jellyfish')
     mr = jellyfish.match_rating_codex(u'Jellyfish')
     
     print('Levenshtein Distance: ', lv)
-    print('Jaro Distance: ', jaro)
+    print('Jaro Distance: ', jd)
+    print('Jaro-Winkler Distance: ', jwd)
+    print('Hamming Distance: ', hd)
     print('Metaphone: ', mp)
     print('Soundex: ', sdx)
     print('NYSIIS: ', nys)
