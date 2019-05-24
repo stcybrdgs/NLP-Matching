67ae7388f05c6053fc3994a75464f1f86aba5fb0
Stacy Bridges <stcybrdgs@gmail.com>
2019-05-24 14:00:16 -0500

    git commit

--- jellyFishTest.py
+++ jellyFishTest.py
@@ -9,23 +9,58 @@ testing jellyfish
 import jellyfish
 
 def main():
-    lv = jellyfish.levenshtein_distance(u'jellyfish', u'smellyfish')
-    jd = jellyfish.jaro_distance(u'jellyfish', u'smellyfish')
-    jwd = jellyfish.jaro_winkler(u'jellyfish', u'smellyfish')
-    hd = jellyfish.hamming_distance(u'jellyfish', u'smellyfish')
-    mp = jellyfish.metaphone(u'Jellyfish')
-    sdx = jellyfish.soundex(u'Jellyfish')
-    nys = jellyfish.nysiis(u'Jellyfish')
-    mr = jellyfish.match_rating_codex(u'Jellyfish')
+    # declare test strings
+    # rem: u prefix is required jellyfish convention
+    str1 = u'Jellyfish' 
+    str2= u'Smellyfish'
     
-    print('Levenshtein Distance: ', lv)
-    print('Jaro Distance: ', jd)
-    print('Jaro-Winkler Distance: ', jwd)
-    print('Hamming Distance: ', hd)
-    print('Metaphone: ', mp)
-    print('Soundex: ', sdx)
-    print('NYSIIS: ', nys)
-    print('Match Rating: ', mr)
+    # test Phonetic Encoding ------------------------------
+    # Metaphone
+    r1 = jellyfish.metaphone(str1)
+    r2 = jellyfish.metaphone(str2)
+    print('Metaphone: ', r1, ", ", r2)
+    
+    # American Soundex
+    r1 = jellyfish.soundex(str1)
+    r2 = jellyfish.soundex(str2)
+    print('Soundex: ', r1, ", ", r2)
+    
+    # NYSIIS
+    r1 = jellyfish.nysiis(str1)
+    r2 = jellyfish.nysiis(str2)
+    print('NYSIIS: ', r1, ", ", r2)
+
+    # Match Rating Codex    
+    r1 = jellyfish.match_rating_codex(str1)
+    r2 = jellyfish.match_rating_codex(str2)
+    print('Match Rating Codex: ', r1, ", ", r2)
+    
+    # test Stemming ---------------------------------------
+    
+    
+    # test String Comparison ------------------------------
+    # Levenshtein Distance
+    r = jellyfish.levenshtein_distance(str1, str2)
+    print('Levenshtein Distance: ', r)
+
+    # Damerau-LevenshteinDistance
+        
+    # Hamming Distance
+    result = jellyfish.hamming_distance(str1, str2)
+    print('Hamming Distance: ', r)
+
+    # Jaro Distance
+    result = jellyfish.jaro_distance(str1, str2)
+    print('Jaro Distance: ', r)
+    
+    # Jaro-Winkler Distance
+    result = jellyfish.jaro_winkler(str1, str2)
+    print('Jaro-Winkler Distance: ', r)
+    
+    # Match Rating Approach (comparison)
+ 
+    
+    
     
     # end program
     print('Done.')
