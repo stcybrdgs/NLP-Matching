cdfeb04c4a55ccb8385b614d962ecfd9610a2ea2
Stacy Bridges <stcybrdgs@gmail.com>
2019-05-28 13:59:25 -0500

    git commit

---
+++ numPyTemp.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+
+x = 1
+print('nuRun: ', x)
+list1 = [1,2,3,4]
+numpArray = np.array(list1)
+print(numpArray.shape)
+print(numpArray.dtype)
+print(np.ones([5,5]))
