a38b5791e3e1afb75d059e4840c9f6ffab20a0fe
Stacy Bridges <stcybrdgs@gmail.com>
2019-05-31 07:17:37 -0500

    git commit

--- recap/controller.py
+++ recap/controller.py
@@ -5,31 +5,16 @@ Created on Fri May 31 04:10:06 2019
 
 controller program for testing spacy nlp features
 
-Phonetic Encoding
-  American Soundex
-  Metaphone
-  NYSIIS
-  Match Rating Approach (codex)
-
-Stemming
-  Porter Stemmer
-
-String Comparison
-  Levenshtein Distance
-  Damerau-Levenshtein Distance
-  Hamming Distance
-  Jaro Distance
-  Jaro-Winkler Distance
-  Match Rating Approach (comparison)
-
-convert us date to uk date
+rem: use case: convert us date to uk date
 
 """
 
-# imports ------------------------------
-# import spacy
+# IMPORTS  ------------------------------
+import jellyfish
+import spacy
 
-# functions-----------------------------
+
+# FUNCTIONS  -----------------------------
 def menu():
     print('\n')
     print('----------------------  MENU  ----------------------')
@@ -44,6 +29,7 @@ def menu():
     print('\nPHONETIC ENCODING:\t STRING COMPARISON:')
     print('6  -  Soundex\t\t 8  -  Levenshtein Distance')
     print('7  -  NYSIIS\t\t 9  -  Jaro-Winkler Distance')
+    print('              \t\t 0  -  Match Rating Codex')
     # print('\n')
     print('----------------------------------------------------')
     # ----  end function  ----
@@ -74,12 +60,48 @@ def matcher():
     # ----  end function  ----
     
 def soundex():
-    print('soundex')
+    tokens = ['Ball Bearing',
+              'bll brng', 
+              'Centrifugal', 
+              'centrifigal', 
+              'PUmp', 
+              'pmp']
+    
+    print('Running SOUNDEX...')
+    
+    # print tokens
+    print('Tokens: ', end='')
+    for i in tokens:
+        print(i,' | ', end='')
+    
+    # printcodes
+    print('\n', end="")
+    print('Codes: ', end='')
+    for i in tokens:
+        print(jellyfish.soundex(i), ' | ', end='')
     
     # ----  end function  ----    
     
 def nysiis():
-    print('nysiis')
+    tokens = ['Ball Bearing',
+              'bll brng', 
+              'Centrifugal', 
+              'centrifigal', 
+              'PUmp', 
+              'pmp']
+    
+    print('Running NYSIIS...')
+    
+    # print tokens
+    print('Tokens: ', end='')
+    for i in tokens:
+        print(i,' | ', end='')
+    
+    # printcodes
+    print('\n', end="")
+    print('Codes: ', end='')
+    for i in tokens:
+        print(jellyfish.nysiis(i), ' | ', end='')
     
     # ----  end function  ----
     
@@ -91,12 +113,64 @@ def levenshtein():
 def jaroWinkler():
     print('jaroWinkler')
     
+def mrc():
+    # english  -----------------------------
+    tokens = ['Ball Bearing',
+              'bll brng', 
+              'Centrifugal', 
+              'centrifigal', 
+              'PUmp', 
+              'pmp']
+    
+    print('Running Match Rating Codex (EN)...')
+    
+    # print tokens
+    print('Tokens: ', end='')
+    for i in tokens:
+        print(i,' | ', end='')    
+        
+    # printcodes
+    print('\n', end="")
+    print('Codes: ', end='')
+    for i in tokens:
+        print(jellyfish.match_rating_codex(i), ' | ', end='')
+        
+    # german  -----------------------------
+    tokens = ['Kugellager',
+              'kugelagr', 
+              'Zentrifugal', 
+              'zentrifkl', 
+              'PUmpe', 
+              'pmp']
+    
+    print('\n\nRunning Match Rating Codex (DE)...')
+    
+    # print tokens
+    print('Tokens: ', end='')
+    for i in tokens:
+        print(i,' | ', end='')    
+        
+    # printcodes
+    print('\n', end="")
+    print('Codes: ', end='')
+    for i in tokens:
+        print(jellyfish.match_rating_codex(i), ' | ', end='') 
+    
+    # print string match comparisons
+    print('\n', end="")
+    print('Comparisons: ', end='')
+    print('Kugellager, kugelagr: ', jellyfish.match_rating_comparison('Kugellager', 'kugelagr'))    
+    print('Zentrifugal, zentrifkl: ', jellyfish.match_rating_comparison('Zentrifugal', 'zentrifkl')) 
+    print('PUmpe, pmp: ', jellyfish.match_rating_comparison('PUmpe', 'pmp'))
+    
     # ----  end function  ----    
     
-# main ---------------------------------
+
+# MAIN  ---------------------------------
+# controller program for nlp functions
 def main():
     # declare menu items
-    menuItems = ['m','1','2','3','4','5','6','7','8','9']
+    menuItems = ['m','1','2','3','4','5','6','7','8','9', '0']
     
     # print menu to console
     menu()
@@ -104,7 +178,7 @@ def main():
     # get menu selection from user
     choice = input('Select a menu item: ')
     
-    # process menu selection
+    # process the user's menu selection
     while choice != 'e':
         # catch invalid user selection
         match = False
@@ -114,7 +188,7 @@ def main():
         
         # process valid user selection
         else:
-            if choice == 'e': break
+            if choice == 'e': break # end program
             if choice == 'm': menu()
             if choice == '1': tokenizer()
             if choice == '2': tagger()
@@ -125,11 +199,11 @@ def main():
             if choice == '7': nysiis()
             if choice == '8': levenshtein()
             if choice == '9': jaroWinkler()
+            if choice == '0': mrc()
         
         # get new user selection
         choice = input('\nSelect a menu item: ')
 
-     
     # end program
     print('\nExit program. Done.')
 
